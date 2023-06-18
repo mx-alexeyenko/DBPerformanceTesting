8ea2d02efaa76c1e276f9bda8fc6603763295d2f
mx-alexeyenko <118298303+mx-alexeyenko@users.noreply.github.com>
2023-06-18 14:32:09 +0300

    Update arango.sh

--- arango.sh
+++ arango.sh
@@ -36,33 +36,33 @@ THREAD_NUMBER=1
 
 
 
-# 1 000 000
-# A
-echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
-$YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloada -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_A7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloada -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoA7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
+# # 1 000 000
+# # A
+# echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
+# $YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloada -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_A7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloada -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoA7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
 
 
-# B
-echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
-$YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloadb -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_B7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloadb -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoB7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
+# # B
+# echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
+# $YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloadb -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_B7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloadb -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoB7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
 
 
-# C
-echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
-$YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloadc -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_C7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloadc -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoC7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
+# # C
+# echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
+# $YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloadc -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_C7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloadc -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoC7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
 
 
-# U
-echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
-$YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloadu -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_U7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloadu -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoU7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
+# # U
+# echo "EXECUTING A6_$THREAD_NUMBER $ATTEMPT ..."
+# $YCSB_PATH/bin/ycsb.sh load arangodb -s -P $YCSB_PATH/workloads/workloadu -p recordcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoINSERT_U7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run arangodb -s -P $YCSB_PATH/workloads/workloadu -p operationcount=1000000 -threads $THREAD_NUMBER -p exportfile=/$YCSB_PATH/results/arango/7/t${THREAD_NUMBER}/arangoU7_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID arangosh --javascript.execute-string "db._dropDatabase(\"ycsb\")" --server.password ""
 
 
 
