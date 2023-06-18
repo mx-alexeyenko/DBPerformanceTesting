36c8de5a6b4a14e706d869bcdbe5c1b0b6a7b184
mx-alexeyenko <118298303+mx-alexeyenko@users.noreply.github.com>
2023-06-18 14:33:49 +0300

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,7 +1,7 @@
 YCSB_PATH=/home/maxim/ycsb-0.17.0
 CONTAINER_ID=ffd7a8a43f9c
-ATTEMPT=3
-THREAD_NUMBER=12
+ATTEMPT=1
+THREAD_NUMBER=1
 
 
 # # 100 000
@@ -56,23 +56,23 @@ THREAD_NUMBER=12
 
 
 
-# 10 000 000
-# A
-$YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloada -p recordcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_A8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloada -p operationcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoA8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
+# # 10 000 000
+# # A
+# $YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloada -p recordcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_A8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloada -p operationcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoA8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
 
-# B
-$YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloadb -p recordcount=5000000 -threads $THREAD_NUMBER  -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_B8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloadb -p operationcount=5000000 -threads $THREAD_NUMBER  -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoB8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
+# # B
+# $YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloadb -p recordcount=5000000 -threads $THREAD_NUMBER  -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_B8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloadb -p operationcount=5000000 -threads $THREAD_NUMBER  -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoB8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
 
-# C
-$YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloadc -p recordcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_C8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloadc -p operationcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoC8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
+# # C
+# $YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloadc -p recordcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_C8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloadc -p operationcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoC8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
 
-# U
-$YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloadu -p recordcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_U8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-$YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloadu -p operationcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoU8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
-docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
+# # U
+# $YCSB_PATH/bin/ycsb.sh load mongodb -s -P $YCSB_PATH/workloads/workloadu -p recordcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoINSERT_U8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# $YCSB_PATH/bin/ycsb.sh run mongodb -s -P $YCSB_PATH/workloads/workloadu -p operationcount=5000000 -threads $THREAD_NUMBER -p mongodb.url="mongodb://127.0.0.1:27017/ycsb?w=1" -p exportfile=/$YCSB_PATH/results/mongo/8/t${THREAD_NUMBER}/mongoU8_T${THREAD_NUMBER}_A${ATTEMPT}.txt
+# docker exec -it $CONTAINER_ID mongo ycsb --eval "db.dropDatabase()"
